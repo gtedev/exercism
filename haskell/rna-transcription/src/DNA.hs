ffd9cade14c78b46eda8a67e4d4555cfb7eca2ff
Gerard Te <gerard.te@orbussoftware.com>
2021-03-07 14:58:54 +0100

    Refactor by using recursive approach.

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -2,32 +2,26 @@ module DNA (toRNA) where
 import qualified Data.Map as Map
 
 
-rnaMap:: Char -> Maybe String
+rnaMap:: Char -> Either Char String
 rnaMap c =
-    Map.lookup c dict
+    let val = Map.lookup c dict
+    in case val of
+
+    Nothing -> Left c
+    Just x -> Right x
+
     where dict = Map.fromList [('G', "C"),('C', "G"),('T', "A"),('A', "U")]
 
-rnaAcc:: (String, Char, Bool) -> Char -> (String, Char, Bool)
-rnaAcc  (acc, prevC, success) c =
-  let val = rnaMap c
-  in case (val, c, (acc, prevC, success)) of
-   (_, _, (_, _, False))  -> (acc, prevC, success)
-   (Nothing, _, (_, _, _)) -> (acc, c, False)
-   (Just x, _, (_, _, _))  ->  (acc ++ x, c, True)
 
+rnaRun:: String -> String -> Either Char String
+rnaRun  acc input =
+  let c  = head input in
+  let val = rnaMap c in
+  case (val, acc, input) of
+  (_, _, []) -> Right acc
+  (Left _, _, _) -> val
+  (Right xxx,_ , _:xs)  ->  rnaRun (acc ++ xxx) xs
 
--- for information: "" = [] =>  that is why the first test works, foldl does nothing as there is no element.
-rnaMapFold:: String -> (String, Char, Bool)
-rnaMapFold = 
-    let initValue = ([], '_' , True)
-    in
-        foldl rnaAcc initValue
-
-
-toEither:: (String, Char , Bool) -> Either Char String
-toEither (acc, _, True) = Right acc
-toEither (_, lastLetter, False) = Left lastLetter
 
 toRNA :: String -> Either Char String
-toRNA  =
-    toEither . rnaMapFold
+toRNA = rnaRun []
