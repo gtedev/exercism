6502623b620fdde230916b58187d6fcb412b45b0
Gerard Te <gerard.te@orbussoftware.com>
2021-03-07 22:28:31 +0100

    Use mapM to improve the code.
    Either Char Char is accumulated to Either Char String !!!!!

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,21 +1,11 @@
 module DNA (toRNA) where
 
-rnaMap:: String -> Either Char String
-rnaMap [] = Right []
-rnaMap ('G':_) = Right "C"
-rnaMap ('C':_) =  Right"G"
-rnaMap ('T':_) = Right "A"
-rnaMap ('A':_) = Right "U"
-rnaMap (x:_) = Left x
-
-rnaRun:: String -> String -> Either Char String
-rnaRun  acc input =
-  let val = rnaMap input in
-  case (val, input) of
-  (_, []) -> Right acc
-  (Left _, _) -> val
-  (Right xxx, _:xs)  ->  rnaRun (acc ++ xxx) xs
-
+rnaMap:: Char -> Either Char Char
+rnaMap 'G' = Right 'C'
+rnaMap 'C' =  Right 'G'
+rnaMap 'T' = Right 'A'
+rnaMap 'A' = Right 'U'
+rnaMap x = Left x
 
 toRNA :: String -> Either Char String
-toRNA = rnaRun []
+toRNA = mapM rnaMap
