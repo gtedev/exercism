96b52c4ae1cc40851702c9a11df37114e16aacf9
Gerard Te <gerard.te@orbussoftware.com>
2021-03-07 15:16:29 +0100

    Replace dictionnary map with simple pattern matching.

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,26 +1,20 @@
 module DNA (toRNA) where
-import qualified Data.Map as Map
-
 
 rnaMap:: String -> Either Char String
 rnaMap [] = Right []
-rnaMap (x:_) =
-    let val = Map.lookup x dict
-    in case val of
-
-    Nothing -> Left x
-    Just xx -> Right xx
-
-    where dict = Map.fromList [('G', "C"),('C', "G"),('T', "A"),('A', "U")]
-
+rnaMap ('G':_) = Right "C"
+rnaMap ('C':_) =  Right"G"
+rnaMap ('T':_) = Right "A"
+rnaMap ('A':_) = Right "U"
+rnaMap (x:_) = Left x
 
 rnaRun:: String -> String -> Either Char String
 rnaRun  acc input =
   let val = rnaMap input in
-  case (val, acc, input) of
-  (_, _, []) -> Right acc
-  (Left _, _, _) -> val
-  (Right xxx,_ , _:xs)  ->  rnaRun (acc ++ xxx) xs
+  case (val, input) of
+  (_, []) -> Right acc
+  (Left _, _) -> val
+  (Right xxx, _:xs)  ->  rnaRun (acc ++ xxx) xs
 
 
 toRNA :: String -> Either Char String
