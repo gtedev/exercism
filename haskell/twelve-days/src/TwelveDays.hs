7e3a71236c24f63a9e7421516d08c7b5192f5c3a
Gerard Te <gerard.te@orbussoftware.com>
2021-03-09 00:43:59 +0100

    Back to my solution.

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -1,40 +1,67 @@
 module TwelveDays (recite) where
 
+data Gift = Gift
+    { name      :: String
+    , quantity  :: String
+    , dayNumber :: String
+    }
+
+toGift:: Int -> Gift
+toGift 1 =  Gift { name = "Partridge in a Pear Tree", quantity="a",      dayNumber = "first" }
+toGift 2 =  Gift { name = "Turtle Doves",             quantity="two",    dayNumber = "second" }
+toGift 3 =  Gift { name = "French Hens",              quantity="three",  dayNumber = "third" }
+toGift 4 =  Gift { name = "Calling Birds",            quantity="four",   dayNumber = "fourth" }
+toGift 5 =  Gift { name = "Gold Rings",               quantity="five",   dayNumber = "fifth" }
+toGift 6 =  Gift { name = "Geese-a-Laying",           quantity="six",    dayNumber = "sixth" }
+toGift 7 =  Gift { name = "Swans-a-Swimming",         quantity="seven",  dayNumber = "seventh"}
+toGift 8 =  Gift { name = "Maids-a-Milking",          quantity="eight",  dayNumber = "eighth"}
+toGift 9 =  Gift { name = "Ladies Dancing",           quantity="nine",   dayNumber = "ninth" }
+toGift 10 = Gift { name = "Lords-a-Leaping",          quantity="ten",    dayNumber = "tenth" }
+toGift 11 = Gift { name = "Pipers Piping",            quantity="eleven", dayNumber = "eleventh" }
+toGift 12 = Gift { name = "Drummers Drumming",        quantity="twelve", dayNumber = "twelfth" }
+toGift _  = Gift { name = "_",                        quantity="_",      dayNumber = "_" }
+
+getGiftDayNumber:: Gift -> String
+getGiftDayNumber (Gift _ _ dayNumber) = dayNumber
+
+getGiftQuantity:: Gift -> String
+getGiftQuantity (Gift _ quantity _) = quantity
+
+getGifName:: Gift -> String
+getGifName (Gift name _ _) = name
+
+toNumberText:: Int -> String
+toNumberText n =
+    getGiftDayNumber $ toGift n
+
+reciteGift:: Int -> String
+reciteGift n =
+    let quantity = getGiftQuantity $ toGift n in
+    let name     = getGifName      $ toGift n in
+    quantity ++ " " ++ name
+
+reciteFirstPart:: Int -> String
+reciteFirstPart n =
+    let numberText = toNumberText n in
+    "On the " ++ numberText ++ " day of Christmas my true love gave to me: "
+
+reciteSecondPart:: Int -> String -> String
+reciteSecondPart n acc
+ | n == 0             = acc
+ | n == 1 && null acc = reciteGiftValue
+ | n == 1             = acc ++ ", and " ++ reciteGiftValue
+ | n > 1 && null acc  = reciteSecondPart (n-1) reciteGiftValue
+ | otherwise          = reciteSecondPart (n-1) ( acc ++ ", " ++  reciteGiftValue)
+
+  where reciteGiftValue = reciteGift n
+
+reciteSentence:: [String] -> Int -> [String]
+reciteSentence acc start =
+    let first    = reciteFirstPart start     in
+    let second   = reciteSecondPart start [] in
+    let sentence = first ++ second ++ "."    in
+    acc ++ [sentence]
+
 recite :: Int -> Int -> [String]
-recite start stop = map verse [start .. stop]
-
-verse :: Int -> String
-verse n = unwords $ prelist n ++ map item [n, (n -1) .. 1]
-
-prelist :: Int -> [String]
-prelist n = ["On the", ordinal n, "day of Christmas my true love gave to me:"]
-
-ordinal :: Int -> String
-ordinal 1 = "first"
-ordinal 2 = "second"
-ordinal 3 = "third"
-ordinal 4 = "fourth"
-ordinal 5 = "fifth"
-ordinal 6 = "sixth"
-ordinal 7 = "seventh"
-ordinal 8 = "eighth"
-ordinal 9 = "ninth"
-ordinal 10 = "tenth"
-ordinal 11 = "eleventh"
-ordinal 12 = "twelfth"
-ordinal _ = ""
-
-item :: Int -> String
-item 1 = "a Partridge in a Pear Tree."
-item 2 = "two Turtle Doves, and"
-item 3 = "three French Hens,"
-item 4 = "four Calling Birds,"
-item 5 = "five Gold Rings,"
-item 6 = "six Geese-a-Laying,"
-item 7 = "seven Swans-a-Swimming,"
-item 8 = "eight Maids-a-Milking,"
-item 9 = "nine Ladies Dancing,"
-item 10 = "ten Lords-a-Leaping,"
-item 11 = "eleven Pipers Piping,"
-item 12 = "twelve Drummers Drumming,"
-item _ = ""
+recite start stop =
+    foldl reciteSentence [] [start..stop]
