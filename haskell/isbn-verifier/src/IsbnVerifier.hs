de9e588048706bbae0740b9d2b7fd586b768fba9
Gerard Te <gerard.te@orbussoftware.com>
2021-03-09 22:27:10 +0100

    Better isbn checks !

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -4,17 +4,19 @@ import Data.Char;
 
 isbnNumbers:: String -> Maybe [Int]
 isbnNumbers s
+   | isEmpty = Nothing
+   | not lastCharIsXorDigit = Nothing
    | not lengthIs10 = Nothing
    | anyXBeforeLast = Nothing
-   | not lastCharIsXorDigit = Nothing
    | otherwise  =
             let numbers =  map (\c -> if c == 'X' then 10 else digitToInt c) digitOrX in
             Just numbers
     where
+       isEmpty = null s
+       lastCharIsXorDigit =  isDigit (last s) || last s == 'X'
        digitOrX = filter (\c -> isDigit c || c == 'X') s
        anyXBeforeLast = 'X' `elem` (tail . reverse) digitOrX
-       lengthIs10 = length digitOrX == 10 
-       lastCharIsXorDigit = isDigit (last digitOrX) || last digitOrX == 'X'
+       lengthIs10 = length digitOrX == 10
 
 
 isbn :: String -> Bool
